bcc85505729570cc91229f7c50a61d42f8577af3
Paige Ashlynn <mx.ashlynn@gmail.com>
2024-08-06 13:11:38 -0700

    Add Tiled project files

---
+++ design/tiles/map_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Map Tiles" tilewidth="8" tileheight="8" tilecount="90" columns="10">
+ <image source="map_tiles.png" width="80" height="72"/>
+</tileset>
